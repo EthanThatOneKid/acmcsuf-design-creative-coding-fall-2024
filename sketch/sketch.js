e44648dc10a0968ea2905488fd094886c749c997
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-11-13 16:57:49 -0800

    convert circles to hearts

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,11 +1,11 @@
-const circles = [];
+const hearts = [];
 const amount = 100;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
   for (let i = 0; i < amount; i++) {
-    circles.push({
+    hearts.push({
       x: random(width),
       y: random(height),
       size: random(10, 50),
@@ -19,35 +19,43 @@ function setup() {
 function draw() {
   background(0, 20);
 
-  circles.forEach((circle) => {
-    const r = map(sin(circle.colorOffset + frameCount * 0.01), -1, 1, 100, 255);
-    const g = map(cos(circle.colorOffset + frameCount * 0.02), -1, 1, 100, 255);
-    const b = map(sin(circle.colorOffset + frameCount * 0.03), -1, 1, 100, 255);
+  hearts.forEach((heart) => {
+    const r = map(sin(heart.colorOffset + frameCount * 0.01), -1, 1, 100, 255);
+    const g = map(cos(heart.colorOffset + frameCount * 0.02), -1, 1, 100, 255);
+    const b = map(sin(heart.colorOffset + frameCount * 0.03), -1, 1, 100, 255);
     fill(r, g, b, 150);
-    ellipse(circle.x, circle.y, circle.size);
+    drawHeart(heart.x, heart.y, heart.size);
 
     // Update position.
-    circle.x += circle.speedX;
-    circle.y += circle.speedY;
+    heart.x += heart.speedX;
+    heart.y += heart.speedY;
 
     // Wrap around edges using radius for better accuracy.
-    const radius = circle.size / 2;
-    if (circle.x < 0 - radius) {
-      circle.x = width + radius;
+    const radius = heart.size / 2;
+    if (heart.x < 0 - radius) {
+      heart.x = width + radius;
     }
 
-    if (circle.x > width + radius) {
-      circle.x = -radius;
+    if (heart.x > width + radius) {
+      heart.x = -radius;
     }
 
-    if (circle.y < 0 - radius) {
-      circle.y = height + radius;
+    if (heart.y < 0 - radius) {
+      heart.y = height + radius;
     }
 
-    if (circle.y > height + radius) {
-      circle.y = -radius;
+    if (heart.y > height + radius) {
+      heart.y = -radius;
     }
   });
+}
+
+function drawHeart(x, y, size) {
+  beginShape();
+  vertex(x, y);
+  bezierVertex(x - size / 2, y - size / 2, x - size, y + size / 3, x, y + size);
+  bezierVertex(x + size, y + size / 3, x + size / 2, y - size / 2, x, y);
+  endShape(CLOSE);
 }
 
 function windowResized() {
